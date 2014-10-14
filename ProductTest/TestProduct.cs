ce6249c866a16b23de4d06f31786832b9dcad832
Jomos <jorgen.olsson@yh.nackademin.se>
2014-10-14 09:39:56 +0200

    Initial commit

---
+++ ProductTest/TestProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductTest
+{
+    [TestClass]
+    public class TestProduct
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
